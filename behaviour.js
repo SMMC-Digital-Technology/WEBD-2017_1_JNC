7d1f714231178367a1ebbe56d3a57190c7c37820
WILLIACA <WILLIACA@mackillop.act.edu.au>
2017-06-07 17:31:43 +1000

    Calculator
    Made calculator work and tested it.

--- behaviour.js
+++ behaviour.js
@@ -11,40 +11,41 @@ function buttonPress()
          //this is how the button works
      
             var number = 0;      
-            if (incomeAmount <16001) 
+            if (incomeAmount <16000) 
             {        
-                taxAmount = "Your tax amount is $" + "$0";  
+                taxAmount = "Your tax amount is $" + "0";  
                 outputLine.value=taxAmount;
             } 
-        else if (incomeAmount <52501) 
+        else if (incomeAmount <52500) 
                 
             {    
-                var number = (incomeAmount - 1600);
-                var taxAmount = number * 0.1;  
-                outputLine.value = "Your tax amount is $" + taxAmount;
+                var number = (incomeAmount - 16000);
+                var taxAmount = (number*0.1);  
+                outputLine.value = "Your2 tax amount is $" + taxAmount;
             } 
-        else if (incomeAmount <113001) 
+        else if (incomeAmount <113000) 
             {
                     var number = (incomeAmount - 52500);
-                    var taxAmount = (number * 0.16) + 3650;
+                    var taxAmount = ((number * 0.16) + 3650);
                     outputLine.value = "Your tax amount is $" + taxAmount;                    
                     
             } 
-        else if (incomeAmount <184501) 
+       else if (incomeAmount <184500) 
             {                
                  var number = (incomeAmount - 113000);
-                 var taxAmount = (number * 0.24) + 13330;
+                 var taxAmount = ((number * 0.24) + 13330);
                 outputLine.value = "Your tax amount is $" + taxAmount;
             } 
-        else
+        else if (incomeAmount >184501)
             {        
-                var number = (incomeAmount - 185400);
-                var taxAmount = (incomeAmount * 0.36) + 30490;
+                var number = (incomeAmount - 184500);
+                var taxAmount = (30490 +(number * 0.36));
                 outputLine.value = "Your tax amount is $" + taxAmount;
             }
         
        
     }
+//plotly graph, ensures that graph appears at the same time as the rest of the content//
 function drawGraph()
 {
     taxThresholds = {
@@ -52,13 +53,13 @@ function drawGraph()
 	    y: [0, 3400, 11250, 22210, 36070, 54070],
         type: 'scatter',
         mode: 'lines+marker',
-        name: 'taxes'
-    };
+        name: 'taxes',
+        };
     
     data=[taxThresholds];
     
     layout = {
-        title: "Title here",
+        title: "Income Tax",
         yaxis: {
             title: "tax",
             zerolinewidth: 1.5,
@@ -67,22 +68,10 @@ function drawGraph()
             title: "income",
             zerolinewidth: 1.5,
         },
-        
+        width: 500,
+        height: 500,
+       
     };
     
     Plotly.newPlot("graph", data, layout);
 }
-
- /*
-         <!---Plotly Graph code--->
-                
-        <div id="tester" style="width:600px;height:250px;"></div>    
-        <!--script-this opens the javascript 'section' of the hmtl file--->
-        <script>
-	       TAX AMOUNT= document.getElementById('taxAmount');
-	       Plotly.plot( taxGraph, [{
-	       x: [0, 50000, 100000, 150000, 200000, 250000],
-	       y: [0, 3400, 11250, 22210, 36070, 54070}], {
-	       margin: { t: 0 } } );
-        </script>
-        */
