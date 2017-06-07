32d0d885b98266d03d7076440b4335b5444b40fc
WILLIACA <WILLIACA@mackillop.act.edu.au>
2017-06-08 09:59:24 +1000

    To 2 decimal place
    Made it to ensure that only one decimal place is shown.

--- behaviour.js
+++ behaviour.js
@@ -8,44 +8,45 @@ function buttonPress()
          //positioning this here means that the variable inputAmount is changed each time a new value is typed in
         var incomeAmount = document.getElementById("incomeAmount").value;
         
-         //this is how the button works, the income amount is taken and it is decided which 
-            // tiers of income are;
+         //this is how the button works, the income amount is taken and it is decided which tax bracket it fits into, the math is then applied to calculate the tax amount, which is outputted. The "taxAmount.toFied(2)" means that only two decimal places are shown, even if there are many more. 
+            // tax brackers are;
                         //0-16000 tax rate of 0,
                         //16001-52500 tax rate of 10% of every dollar over 16000,
                         //52501-113000 tax rate of 16% of every dollar over 52500+3650, 
                         //113001-184500 tax rate of 24% of every dollar over 113000 + 13330
                         //184501 + tax rate of 36% of every dollar over 184500 + 30490
+        
             var number = 0;      
             if (incomeAmount <16000) 
             {        
                 taxAmount = "Your tax amount is $" + "0";  
-                outputLine.value=taxAmount;
+                outputLine.value=taxAmount.toFixed(2);
             } 
         else if (incomeAmount <52500) 
                 
             {    
                 var number = (incomeAmount - 16000);
                 var taxAmount = (number*0.1);  
-                outputLine.value = "Your tax amount is $" + taxAmount;
+                outputLine.value = "Your tax amount is $" + taxAmount.toFixed(2);
             } 
         else if (incomeAmount <113000) 
             {
                     var number = (incomeAmount - 52500);
                     var taxAmount = ((number * 0.16) + 3650);
-                    outputLine.value = "Your tax amount is $" + taxAmount;                    
+                    outputLine.value = "Your tax amount is $" + taxAmount.toFixed(2);                    
                     
             } 
        else if (incomeAmount <184500) 
             {                
                  var number = (incomeAmount - 113000);
                  var taxAmount = ((number * 0.24) + 13330);
-                outputLine.value = "Your tax amount is $" + taxAmount;
+                outputLine.value = "Your tax amount is $" + taxAmount.toFixed(2);
             } 
         else if (incomeAmount >184501)
             {        
                 var number = (incomeAmount - 184500);
                 var taxAmount = (30490 +(number * 0.36));
-                outputLine.value = "Your tax amount is $" + taxAmount;
+                outputLine.value = "Your tax amount is $" + taxAmount.toFixed(2);
             }
         
        
@@ -77,7 +78,7 @@ function drawGraph()
         height: 500,
        
     };
-//plot graph. 
+//tells to plot graph. 
     
     Plotly.newPlot("graph", data, layout);
 }
